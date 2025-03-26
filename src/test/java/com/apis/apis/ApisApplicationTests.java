1c81798e8366b83a5f3b6d7dc3a11b07839e9491
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-03-26 19:34:37 +0100

    Started apis

---
+++ src/test/java/com/apis/apis/ApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apis.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
